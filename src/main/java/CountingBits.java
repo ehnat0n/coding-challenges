2c835355e844ae056163412d32fcdecf11a9a414
Dmitry Igumnov <53914429+ehnat0n@users.noreply.github.com>
2022-03-02 12:30:30 -0700

    Added O(n) solution for CountingBits problem

--- src/main/java/CountingBits.java
+++ src/main/java/CountingBits.java
@@ -8,7 +8,26 @@ public class CountingBits {
 
     0 <= n <= 10^5
      */
+
+    /*
+    Based on observation that every even number n is n/2 number shifted left with identical pattern.
+    Odd number is previous even + 1.
+    O(n)/O(n)
+     */
     public int[] countBits(int n) {
+        int[] result = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 0) result[i] = result[i / 2];
+            else result[i] = result[i - 1] + 1;
+        }
+        return result;
+    }
+
+    /*
+    Here we reset last 1-bit to 0, can also simply shift right >> and count 1-bits at the end.
+    O(n*logn)/O(n)
+     */
+    public int[] countBits2(int n) {
         int[] result = new int[n + 1];
         int count;
         int num;
@@ -16,7 +35,7 @@ public class CountingBits {
             count = 0;
             num = i;
             while (num > 0) {
-                num &= num - 1; //here we reset last 1-bit to 0, can also simply shift right and count 1-bits at the end
+                num &= num - 1;
                 count++;
             }
             result[i] = count;
